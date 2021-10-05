6d6b7f774087192f6cd8f4f814d0f5eddceb991a
julybell <julybell16@gmail.com>
2021-10-06 00:54:54 +0300

    node part 2

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var url = require('url');
+var fl = 'http://localhost:3000/forbidden?secret=true';
+var fullLink = url.parse(fl, true);
+
+let accessStatus = fullLink.query.secret;
+console.log(accessStatus);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,8 +28,6 @@ app.use(function(req, res, next){
   }
 });
 
-let accessStatus = true;
-
 app.use(function(req, res, next){
   if(req.url == '/forbidden?secret=' + accessStatus){
     res.end('Access approved');
@@ -30,29 +35,6 @@ app.use(function(req, res, next){
     res.end('Access denied');
   }
 })
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
